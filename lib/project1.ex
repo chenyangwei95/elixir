ff53d0e5580a996800d323268c392adffdc37ee7
chenyangwei95 <chenyangwei95@gmail.com>
2018-09-07 22:48:17 -0400

    new elixir project

---
+++ lib/project1.ex
@@ -0,0 +1,22 @@
+defmodule Project1 do
+  @moduledoc """
+  Documentation for Project1.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Project1.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+
+  def main() do
+    IO.puts "Hello, word!"
+  end
+end
